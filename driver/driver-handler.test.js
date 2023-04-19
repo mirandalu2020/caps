7e3785f08b6877c391c31f5d7a77e607d8603212
mirandalu2020 <yuanling1007@hotmail.com>
2023-04-19 10:55:49 -0700

    driver test completed

--- driver/driver-handler.test.js
+++ driver/driver-handler.test.js
@@ -1,19 +1,39 @@
 'use strict';
 
 require('./handler');
-const { emitter } = require('./../eventPool');
+// const { emitter } = require('./../eventPool');
+const { handlePickUp, joinRoom } = require('./handler');
 
 
 describe('Testing driver handler to listen for and emitting events', ()=>{
 
-  test('When the an pick-up event occurs, emit event', ()=>{
+  test('Can the driver handle a pickup event', ()=>{
     let payload=
     { store: '1-206-flowers',
       orderID: 'e3669048-7313-427b-b6cc-74010ca1f8f0',
       customer: 'Jamal Braun',
       address: 'Schmittfort, LA' };
-    console.log = jest.fn();
-    emitter.emit('pickup', payload);
-    expect(console.log).toHaveBeenCalledTimes(1);
+
+    let socket = {
+      emit: jest.fn(),
+    }
+
+    handlePickUp(socket, payload);
+    expect(socket.emit).toHaveBeenCalled();
+  })
+
+  test('Can the driver join a room', ()=>{
+    let socket = {
+      emit: jest.fn(),
+    }
+
+    let payload=
+    { store: '1-206-flowers',
+      orderID: 'e3669048-7313-427b-b6cc-74010ca1f8f0',
+      customer: 'Jamal Braun',
+      address: 'Schmittfort, LA' };
+
+    joinRoom(socket, payload, 'test-room');
+    expect(socket.emit).toHaveBeenCalled();
   })
 })
--- driver/handler.js
+++ driver/handler.js
@@ -1,24 +1,39 @@
 'use strict';
 
-const { io } = require('socket.io-client');
-const SERVER_URL = process.env.SERVER_URL || 'http://localhost:3001';
+// const { io } = require('socket.io-client');
+// const SERVER_URL = process.env.SERVER_URL || 'http://localhost:3001';
 
-const { emitter, eventPool } = require('./../eventPool');
+// const { emitter, eventPool } = require('./../eventPool');
+// const { payload } = require('../vendor/handler');
 
-let capsSocket = io(SERVER_URL + '/caps');
+// let capsSocket = io(SERVER_URL + '/caps');
 
 // capsSocket.emit('join-group', payload);
 // console.log(eventPool);
 
-capsSocket.on(eventPool[0], (payload) =>{
+// capsSocket.on(eventPool[0], (payload) =>{
 
-  capsSocket.emit('join-group', payload);
-  console.log('caps socket on')
+//   capsSocket.emit('join-group', payload);
+//   // console.log('caps socket on')
 
-  console.log(`DRIVER: picked up ${payload['orderId']}`)
-  capsSocket.emit(eventPool[1], payload);
+//   console.log(`DRIVER: picked up ${payload['orderId']}`)
+//   capsSocket.emit(eventPool[1], payload);
 
-  console.log(`${payload.orderId} in transit`);
-  capsSocket.emit(eventPool[2], payload);
+//   console.log(`${payload.orderId} in transit`);
+//   capsSocket.emit(eventPool[2], payload);
 
-});
+// });
+
+module.exports = {
+  joinRoom: function(socket, payload, roomName) {
+    socket.emit(roomName, payload);
+    console.log('caps socket on')
+  },
+  handlePickUp: function(socket, payload) {
+      socket.emit('in-transit', payload);
+      console.log(`DRIVER: picked up ${payload['orderId']}`)
+      socket.emit('delivered', payload);
+      console.log(`${payload.orderId} in transit`);
+  }
+
+}
